638139b23350172e3459322be5ad4557b12d4fc3
Andre Brown <andreups@msn.com>
2021-05-21 12:53:43 -0400

    added comments and banner

--- toCelsius.js
+++ toCelsius.js
@@ -1,17 +1,21 @@
 "use strict";
 
 window.onload = function() {
+    // Link convert button to JavaScript event
     const toCelstBtn = document.getElementById("toCelsBtn");
     toCelstBtn.onclick = toCelsBtnClicked;
 }
 
 
 function toCelsBtnClicked() {
+    // Extract Fahrenheit input from fInput field
     const fInputField = document.getElementById("fInput");
     let fInput = Number(fInputField.value);
 
+    // Calculate temperature in Celsius from Fahrenheit input
     let celsTemp = (fInput - 32) * 5/9;
 
+    // Assign result field and display in html field.
     const cResultField = document.getElementById("cResult");
     cResultField.value = celsTemp.toFixed(2); 
 }
--- toFahrenheit.js
+++ toFahrenheit.js
@@ -1,16 +1,20 @@
 "use strict";
 
 window.onload = function() {
+    // Link convert button to JavaScript event
     const toFahrBtn = document.getElementById("toFahrBtn");
     toFahrBtn.onclick = toFahrBtnClicked;
 }
 
 function toFahrBtnClicked() {
+    // Extract Celsius input from cInput field
     const cInputField = document.getElementById("cInput");
     let cInput = Number(cInputField.value);
-
+    
+    // Calculate temperature in Fahrenheit from Celsius input
     let fahrTemp = (cInput * 9/5) + 32;
 
+    // Assign result field and display in html field.
     const fResultField = document.getElementById("fResult");
     fResultField.value = fahrTemp.toFixed(2); 
 }
